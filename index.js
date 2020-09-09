93478b2c7e243043bbb6c80476d521127d042992
evan.pradipta <evan@qlue.id>
2020-09-09 12:20:30 +0700

    made api to obtain traffic conditions

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const express = require('express')
 const delay = require("delay");
 const request = require('request');
 const axios = require('axios');
-const addressCounter1 = 500;
-const addressCounter2 = 501;
-const addressCounter3 = 502;
-const addressCounter4 = 503;
+
+const addressMode = [1040, 1014, 1018] //dynamic, hazard, regular
+const addressCounter = [51, 501, 502, 503];
 const addressCounterReg = 505;
 
 const app = express()
@@ -37,53 +36,54 @@ var statusGreenLight = [false, false, false, false]
 app.use(express.json())
 var address = require('address');
 var myip = address.ip();
-var a = 0;
-var time = [0,0,0]
+var time = [0, 0, 0]
 
 //========DEFAULT TIME VARIABLE ========//
-var timerDensity = [10, 30, 50] // individual time set for low-med-high congestion
+var timerDensity = [10, 15, 20] // individual time set for low-med-high congestion
 
 //======== LANE CONGESTION =========//
 var laneDensity = [1, 1, 1, 1] // lane density where 0:low, 1:med, 2:high
 var greenLightValue = [0, 0, 0, 0] //density values that has been swapped with time variable
+
 var greenLightCounter = [0, 0, 0, 0]
+var redLightCounter = [0, 0, 0, 0]
+var yellowLightCounter = [0, 0, 0, 0]
 connectPLC();
 
 
+
+
 app.get('/dynamicstatus', (req, res) => { //reads whether dynamic mode is on or off
-  respReadPLC(1013, res);
+  respReadPLC(addressMode[0], res);
   //res.send(readPLC(1013))
   console.log('Done on http');
 })
 
 app.get('/hazardstatus', (req, res) => { //reads whether hazard mode is on or off
-  respReadPLC(1014, res);
+  respReadPLC(addressMode[1], res);
   console.log('Done on http');
 })
 
 app.get('/regularstatus', (req, res) => { //reads whether regular mode is on or off
-  respReadPLC(1018, res);
+  respReadPLC(addressMode[2], res);
   console.log('Done on http');
 })
 
-app.get('/trafficstatus', (req, res) => {
-  readCounters(addressCounterReg, res);
-  console.log('Done on http');
-})
-
-function readCounters(addr, res){
-client.readInputRegisters(addr, 1, function(err, data){
-console.log(data.data);
-res.send(data.data);
-});
+function respReadPLC(addr, res) { //checks for status of mode wether on or not
+  client.readCoils(addr,3, function(err, data) {
+    console.log(data);
+    res.send(data.data[0-3]);
+  });
 }
 
-function respReadPLC(addr, res) {
-  // read the values of 10 registers starting at address 0
-  // on device number 1. and log the values to the console.
-  client.readHoldingRegisters(addr, 1, function(err, data) {
+app.get('/trafficstatus', (req, res) => {
+  getAllData(res);
+})
+
+function getAllData(res) {
+   client.readHoldingRegisters(addressCounter[0], 8, function(err, data) {
+  res.send(data.data);
     console.log(data.data);
-    res.send(data.data);
   });
 }
 
@@ -120,50 +120,29 @@ function updatePreset(level, time) {
 }
 
 
-//==============//
-// DYNAMIC MODE //
-//==============//
+//switch to dynamic mode
 app.get('/dynamic', (req, res) => {
-  enableDynamic(1013, res);
+  enableMode(1013, res);
 })
-
-function enableDynamic(addr, res) {
-  client.writeCoil(addr, 1);
-  setTimeout(function() {
-    client.writeCoil(addr, 0);
-    res.send('enabled');
-  }, 1000);
-}
-
-//=============//
-// HAZARD MODE //
-//=============//
+//switch to hazard mode
 app.get('/hazard', (req, res) => {
-  enableHazard(1014, res);
+  enableMode(1014, res);
 })
-
-function enableHazard(addr, res) {
-  client.writeCoil(addr, 1);
-  setTimeout(function() {
-    client.writeCoil(addr, 0);
-    res.send('enabled');
-  }, 1000);
-}
-
-//==============//
-// REGULAR MODE //
-//==============//
+//switch to regular mode
 app.get('/regular', (req, res) => {
-  enableRegular(1015, res);
+  enableMode(1015, res);
 })
 
-function enableRegular(addr, res) {
+function enableMode(addr, res) {
   client.writeCoil(addr, 1);
   setTimeout(function() {
     client.writeCoil(addr, 0);
     res.send('enabled');
   }, 1000);
 }
+
+
+
 
 app.listen(port, () => {
   console.log(`Modbus Middleware listening at http://${myip}:${port}`)
@@ -197,7 +176,7 @@ function readLoopCoilLightStatus(i, j, im, jm, functionx) {
   addr = addrCoilLight[i][j];
   client.readCoils(addr, 1, function(err, data) {
     //console.log(err);
-    if(err){
+    if (err) {
       readLoopCoilLightStatus(i, j, im, jm, functionx);
       return;
     }
@@ -227,33 +206,7 @@ function showStatusCoilLight() {
   console.log(statusCoilLight);
   console.log('density status');
   console.log(laneDensity);
-  console.log('green light counter');
-  console.log(greenLightCounter);
 
-  obtainCounterStatus(addressCounterReg, addressCounter1, addressCounter2, addressCounter3, addressCounter4);
-}
-
-function obtainCounterStatus(addrreg, addrdyn1, addrdyn2, addrdyn3, addrdyn4){
-  client.readInputRegisters(addrreg, 1, function(err, datar){
-  //console.log(datar.data);
-
-  });
-  // client.readInputRegisters(addrdyn1, 1, function(err, datad1){
-  // //console.log(datad1.data);
-  // greenLightCounter[0] = datad1.data ;
-  // });
-  // client.readInputRegisters(addrdyn2, 1, function(err, datad2){
-  // //console.log(datad2.data);
-  // greenLightCounter[1] = datad2.data ;
-  // });
-  // client.readInputRegisters(addrdyn3, 1, function(err, datad3){
-  // //console.log(datad3.data);
-  // greenLightCounter[2] = datad3.data ;
-  // });
-  // client.readInputRegisters(addrdyn4, 1, function(err, datad4){
-  // //console.log(datad4.data);
-  // greenLightCounter[3] = datad4.data ;
-  // });
 }
 
 //====================//
@@ -262,7 +215,7 @@ function obtainCounterStatus(addrreg, addrdyn1, addrdyn2, addrdyn3, addrdyn4){
 function obtainCongestion() {
 
   if (statusCoilLight[3][1] == true) { // if yellow light in lane4 is on
-    client.writeCoil(1006, 1);
+    client.writeCoil(1006, 1); // send heatbeat to PLC
     setTimeout(function() {
       client.writeCoil(1006, 0);
     }, 500);
@@ -275,11 +228,7 @@ function obtainCongestion() {
         .catch(error => {
           console.log(error);
         });
-
-      statusYellowLight[0] = false;
-      statusYellowLight[3] = true;
-      statusYellowLight[1] = false;
-      statusYellowLight[2] = false;
+      statusYellowLight = [false, false, false, true]
     }
   } else if (statusCoilLight[0][1] == true) { //if yellow light in lane 1 is on
     if (statusCoilLight[0][1] !== statusYellowLight[0]) { //and state of yellow light is originlly off
@@ -291,10 +240,7 @@ function obtainCongestion() {
         .catch(error => {
           console.log(error);
         });
-      statusYellowLight[1] = false;
-      statusYellowLight[0] = true;
-      statusYellowLight[3] = false;
-      statusYellowLight[2] = false;
+      statusYellowLight = [true, false, false, false]
     }
   } else if (statusCoilLight[1][1] == true) { //lane3
     if (statusCoilLight[1][1] !== statusYellowLight[1]) {
@@ -306,10 +252,7 @@ function obtainCongestion() {
         .catch(error => {
           console.log(error);
         });
-      statusYellowLight[2] = false;
-      statusYellowLight[0] = false;
-      statusYellowLight[1] = true;
-      statusYellowLight[3] = false;
+      statusYellowLight = [false, true, false, false]
     }
   } else if (statusCoilLight[2][1] == true) { //lane4
     if (statusCoilLight[2][1] !== statusYellowLight[2]) {
@@ -321,58 +264,41 @@ function obtainCongestion() {
         .catch(error => {
           console.log(error);
         });
-      statusYellowLight[3] = false;
-      statusYellowLight[0] = false;
-      statusYellowLight[1] = false;
-      statusYellowLight[2] = true;
+      statusYellowLight = [false, false, true, false]
     }
   }
 }
-
 
 //=================//
 // UPDATE PLC TIME // when green light start
 //=================//
 function updatePLCTimer() {
   if (statusCoilLight[0][2] == true) { // if green light in lane 1 is on
-    client.readInputRegisters(addressCounter1, 1, function(err, datad1){
-    greenLightCounter[0] = (timerDensity[laneDensity[0]]-(datad1.data)-1) ;
-    });
     if (statusCoilLight[0][2] !== statusGreenLight[0]) {
-      client.writeRegister(0002, timerDensity[laneDensity[0]]);
-      console.log('lane1 data updated');
+      client.writeRegister(0002, timerDensity[laneDensity[0]]); // update timer value
+      //console.log('lane1 data updated');
       statusGreenLight = [true, false, false, false];
     }
   } else if (statusCoilLight[1][2] == true) {
-    client.readInputRegisters(addressCounter2, 1, function(err, datad2){
-    greenLightCounter[1] = (timerDensity[laneDensity[1]]-(datad2.data)-1) ;
-    });
+
     if (statusCoilLight[1][2] !== statusGreenLight[1]) {
       client.writeRegister(0003, timerDensity[laneDensity[1]]);
-      console.log('lane2 data updated');
+      //console.log('lane2 data updated');
       statusGreenLight = [false, true, false, false];
     }
   } else if (statusCoilLight[2][2] == true) {
-    client.readInputRegisters(addressCounter3, 1, function(err, datad3){
-    greenLightCounter[2] = (timerDensity[laneDensity[2]]-(datad3.data)-1) ;
-    });
     if (statusCoilLight[2][2] !== statusGreenLight[2]) {
       client.writeRegister(0004, timerDensity[laneDensity[2]]);
-      console.log('lane3 data updated');
+      //console.log('lane3 data updated');
       statusGreenLight = [false, false, true, false];
     }
 
   } else if (statusCoilLight[3][2] == true) {
-    client.readInputRegisters(addressCounter4, 1, function(err, datad4){
-    greenLightCounter[3] = (timerDensity[laneDensity[3]]-(datad4.data)-1) ;
-    });
     if (statusCoilLight[3][2] !== statusGreenLight[3]) {
       client.writeRegister(0005, timerDensity[laneDensity[3]]);
-      console.log('lane4 data updated');
+      //console.log('lane4 data updated');
       statusGreenLight = [false, false, false, true];
     }
-  }else{
-    greenLightCounter = [0,0,0,0]
   }
 }
 
